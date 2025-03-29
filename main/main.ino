1bd3cdf71be0771ad63d23a4c3ece09611492004
Chill-Sam <jonathan@wahrenberg.com>
2025-03-29 11:16:18 +0100

    Reformatted main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,24 @@
-#include <AHRS.h>
+#include "AHRS.h"
 
 AHRS ahrs;
 
 volatile float pitch, roll, yaw;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting")
-  ahrs.init()
+    Serial.begin(115200);
+    Serial.println("Starting");
+    ahrs.init();
 }
 
 void getOrientation() {
-  pitch = ahrs.pitch;
-  roll = ahrs.roll;
-  yaw = ahrs.yaw;
+    pitch = ahrs.pitch;
+    roll = ahrs.roll;
+    yaw = ahrs.yaw;
 }
 
 void loop() {
-  getOrientation();
+    getOrientation();
 
-  Serial.println("pitch:" + String(pitch) + ",roll:" + String(roll) + ",yaw:" + String(yaw));
+    Serial.println("pitch:" + String(pitch) + ",roll:" + String(roll) +
+                   ",yaw:" + String(yaw));
 }
