7dde90c02f5bf7e83f3c84d47326701fc9bf3c01
Chill-Sam <jonathan@wahrenberg.com>
2025-03-29 11:14:24 +0100

    Reformat

--- main/src/AHRS/AHRS.h
+++ main/src/AHRS/AHRS.h
@@ -1,19 +1,19 @@
-#include "MPUHandler.h"
-#include <QMC5883LCompass.h>
+#include "MPU6000.h"
 #include <MadgwickAHRS.h>
+#include <QMC5883LCompass.h>
 
 class AHRS {
-public:
-  volatile float pitch, roll, yaw;
+  public:
+    volatile float pitch, roll, yaw;
 
-  AHRS();
+    AHRS();
 
-  void init();
+    void init();
 
-private:
-  QMC5883LCompass compass;
-  MPUHandler mpu;
-  Madgwick filter;
+  private:
+    QMC5883LCompass compass;
+    MPU6000 mpu;
+    Madgwick filter;
 
-  void sensorTask(void* pvParameters);
+    void sensorTask(void *pvParameters);
 }
---
+++ main/src/AHRS/MPU6000.h
@@ -0,0 +1,24 @@
+#include <array>
+#include <cstdint>
+
+class MPU6000 {
+  private:
+    float accelXOffset, accelYOffset, accelZOffset, gyroXOffset, gyroYOffset,
+        gyroZOffset = 0; //< Variables for offsets
+
+    void writeRegister(uint8_t reg, uint8_t value);
+    uint8_t readRegister(uint8_t reg);
+
+  public:
+    MPU6000();
+
+    void initializeMPU();
+
+    void getRawData(int16_t &rawAccelX, int16_t &rawAccelY, int16_t &rawAccelZ,
+                    int16_t &rawGyroX, int16_t &rawGyroY, int16_t &rawGyroZ);
+    void getData(float &ax, float &ay, float &az, float &gx, float &gy,
+                 float &gz);
+
+    void calibrateAccelerometer();
+    void calibrateGyroscope();
+};
