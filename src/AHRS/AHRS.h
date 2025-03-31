0746bcec31361a6782204e4e5ad459feb42b9cf6
Chill-Sam <jonathan@wahrenberg.com>
2025-03-31 11:10:17 +0200

    Moved orientation struct to AHRS

--- src/AHRS/AHRS.h
+++ src/AHRS/AHRS.h
@@ -4,6 +4,12 @@
 #include <MadgwickAHRS.h>
 #include <QMC5883LCompass.h>
 
+struct Orientation {
+    float pitch = 0.0F;
+    float roll = 0.0F;
+    float yaw = 0.0F;
+};
+
 class AHRS {
   public:
     volatile float pitch = 0, roll = 0, yaw = 0;
