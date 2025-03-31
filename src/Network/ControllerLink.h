480d2eae228caf6201129d5d34026541784cbc16
Chill-Sam <jonathan@wahrenberg.com>
2025-03-31 17:59:56 +0200

    Added Netwrok

--- src/Network/ControllerLink.h
+++ src/Network/ControllerLink.h
@@ -1,7 +1,6 @@
 // NOTE: Class that connects, sends and recieves commands via internet
 
 // TODO:
-// - Connect to internet
 // - Recieve data from websocket
 // - Failsafe on network error
 // - Able to arm / disarm
@@ -9,6 +8,9 @@
 // - Send telemetery (throttle, arming state)
 
 #pragma once
+
+#include "Safety/DroneState.h"
+#include "TCS/ThrustController.h"
 
 struct Input {
     float pitch;
@@ -21,6 +23,10 @@ class ControllerLink {
   public:
     ControllerLink(const char *ssid, const char *password,
                    const char *websocket);
+
+    void begin();
+    void onRecieve();
+    void sendTelemetry(DroneState state, TCSState thrustState);
 
   private:
     const char *ssid;
