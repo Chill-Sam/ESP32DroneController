c760c4429f8bb99f88e04805f8cd2255f0760011
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2025-04-23 00:02:52 +0000

    new types files

---
+++ firmware/src/types/DroneState.h
@@ -0,0 +1,14 @@
+#include "ControlData.h"
+#include "FlightData.h"
+
+struct DroneState {
+    // Onboard Controlled
+    FlightMode flightMode = FlightMode::DISARMED;
+    Orientation orientation;
+    ThrottleState throttleState;
+    ArmState armState;
+
+    // Remote Controlled
+    SetpointState setpointState;
+    RCArmingState rcArmingState;
+};
